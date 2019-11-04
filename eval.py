9f86efa313d72f56e116a20fef1261eb18400933
xu-jerry <54759887+xu-jerry@users.noreply.github.com>
2019-11-03 21:17:14 -0800

    Update eval.py

--- eval.py
+++ eval.py
@@ -1,3 +1,12 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
 PATH = 'model'
 
 net.load_state_dict(torch.load(PATH))
